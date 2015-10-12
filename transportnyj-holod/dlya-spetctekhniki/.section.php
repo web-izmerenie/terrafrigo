c93b95f86a3da8b317e62b2ed35d1685c1c3f07f
Simple User <u442824@gen126.hs.shared.masterhost.ru>
2015-10-12 12:17:38 +0300

    edit content

--- transportnyj-holod/dlya-spetctekhniki/.section.php
+++ transportnyj-holod/dlya-spetctekhniki/.section.php
@@ -1,11 +1,5 @@
 <?
-<<<<<<< HEAD
-$sSectionName = "Кондиционеры для коммерческого транспорта";
-$arDirProperties = array(
-=======
 $sSectionName = "Кондиционеры для спецтехники";
 $arDirProperties = Array(
->>>>>>> dc2f2f5a79a4ad706c0e0e0355b05c91d445ad22
-
 );
 ?>
